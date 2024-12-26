06acdc9d9595f1b7445b2a55eac90e5ae2fc3b9b
vamosinternet <indicadores@vamos.com.vc>
2024-12-26 10:13:18 -0300

    Update video-data.js

--- videos/js/video-data.js
+++ videos/js/video-data.js
@@ -1,56 +1,73 @@
 const videoModules = {
-    leadershipCreative: {
-        name: "Liderança Criativa",
-        thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png",
+    onboarding: {
+        name: "Onboarding",
+        thumbnail: "https://example.com/onboarding-thumbnail.jpg",
+        videos: []
+    },
+    pdl: {
+        name: "PDL",
+        thumbnail: "https://example.com/pdl-thumbnail.jpg",
         videos: [
             {
-                id: 4,
+                id: 1,
                 title: "Apresentações Criativas",
                 url: "https://video.wixstatic.com/video/2b43fc_af7238477d4c4abd936ca0de3b25f5e2/1080p/mp4/file.mp4",
                 thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
             },
             {
-                id: 5,
-                title: "Identidade Visual",
-                url: "https://video.wixstatic.com/video/2b43fc_ec2e1d6ed8844d89ad0b2a427ed48e4b/1080p/mp4/file.mp4",
-                thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
-            },
-            {
-                id: 6,
-                title: "Exercício prático",
-                url: "https://video.wixstatic.com/video/2b43fc_89ae9380b33640be8691f11442944215/1080p/mp4/file.mp4",
-                thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
-            },
-            {
-                id: 7,
-                title: "Exercício prático - parte 2",
-                url: "https://video.wixstatic.com/video/2b43fc_89ae9380b33640be8691f11442944215/1080p/mp4/file.mp4",
-                thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
-            }
-        ]
-    },
-    it: {
-        name: "IT",
-        thumbnail: "https://static.wixstatic.com/media/2b43fc_944fd660f85c431f9a7ffc9751bd82f0~mv2.png",
-        videos: [
-            {
-                id: 8,
-                title: "Vídeo Passo a Passo para Preenchimento do Formulário de Feedback",
+                id: 2,
+                title: "Passo a Passo para Preenchimento do Formulário de Feedback",
                 url: "https://video.wixstatic.com/video/2b43fc_7a852b1a8035442f88f0759cc0333e59/720p/mp4/file.mp4",
                 thumbnail: "https://static.wixstatic.com/media/2b43fc_944fd660f85c431f9a7ffc9751bd82f0~mv2.png"
             }
         ]
     },
+    saudeSeguracaBemEstar: {
+        name: "+Saúde, Segurança e Bem-Estar",
+        thumbnail: "https://example.com/saude-seguranca-bem-estar-thumbnail.jpg",
+        videos: []
+    },
+    vendas: {
+        name: "+Vendas",
+        thumbnail: "https://example.com/vendas-thumbnail.jpg",
+        videos: []
+    },
+    escolaDeLideres: {
+        name: "Escola de Líderes",
+        thumbnail: "https://example.com/escola-de-lideres-thumbnail.jpg",
+        videos: []
+    },
+    telecom: {
+        name: "Telecom",
+        thumbnail: "https://example.com/telecom-thumbnail.jpg",
+        videos: []
+    },
+    sucessoDoCliente: {
+        name: "Sucesso do Cliente",
+        thumbnail: "https://example.com/sucesso-do-cliente-thumbnail.jpg",
+        videos: []
+    },
+    compliance: {
+        name: "Compliance",
+        thumbnail: "https://example.com/compliance-thumbnail.jpg",
+        videos: []
+    },
+    clubeDoLivro: {
+        name: "Clube do Livro",
+        thumbnail: "https://example.com/clube-do-livro-thumbnail.jpg",
+        videos: []
+    },
     pontoMais: {
         name: "Ponto Mais",
-        thumbnail: "https://static.wixstatic.com/media/2b43fc_590d1ba0e40c451a9e6cefa9cf9c451f~mv2.png",
+        thumbnail: "https://static.wixstatic.com/media/2b43fc_b927c7053fbd4bad975ddb156ee82e63~mv2.jpg",
         videos: [
             {
-                id: 9,
+                id: 3,
                 title: "No Ponto Certo Com Ponto Mais",
                 url: "https://video.wixstatic.com/video/2b43fc_d642cb5dd7dd4ef593539bc435f34654/720p/mp4/file.mp4",
-                thumbnail: "https://static.wixstatic.com/media/2b43fc_590d1ba0e40c451a9e6cefa9cf9c451f~mv2.png"
+                thumbnail: "https://static.wixstatic.com/media/2b43fc_b927c7053fbd4bad975ddb156ee82e63~mv2.jpg"
             }
         ]
     }
 };
+
