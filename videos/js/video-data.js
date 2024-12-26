e22eed66832a3c7fa641ab758daa8ede8cab7457
vamosinternet <indicadores@vamos.com.vc>
2024-12-26 10:16:58 -0300

    Update video-data.js

--- videos/js/video-data.js
+++ videos/js/video-data.js
@@ -16,6 +16,24 @@ const videoModules = {
             },
             {
                 id: 2,
+                title: "Identidade Visual",
+                url: "https://video.wixstatic.com/video/2b43fc_ec2e1d6ed8844d89ad0b2a427ed48e4b/1080p/mp4/file.mp4",
+                thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
+            },
+            {
+                id: 3,
+                title: "Exercício prático",
+                url: "https://video.wixstatic.com/video/2b43fc_89ae9380b33640be8691f11442944215/1080p/mp4/file.mp4",
+                thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
+            },
+            {
+                id: 4,
+                title: "Exercício prático - parte 2",
+                url: "https://video.wixstatic.com/video/2b43fc_89ae9380b33640be8691f11442944215/1080p/mp4/file.mp4",
+                thumbnail: "https://static.wixstatic.com/media/2b43fc_b605a77ab0f04eb482b3682c12d38a0c~mv2.png"
+            },
+            {
+                id: 5,
                 title: "Passo a Passo para Preenchimento do Formulário de Feedback",
                 url: "https://video.wixstatic.com/video/2b43fc_7a852b1a8035442f88f0759cc0333e59/720p/mp4/file.mp4",
                 thumbnail: "https://static.wixstatic.com/media/2b43fc_944fd660f85c431f9a7ffc9751bd82f0~mv2.png"
